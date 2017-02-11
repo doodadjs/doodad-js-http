6fd64ac49e0090e26e0d66cc156861930ce30cf8
Claude Petit <doodadjs@gmail.com>
2017-02-11 15:01:49 -0500

    Apply changes of "files.Url".

--- src/server/Server_Http.js
+++ src/server/Server_Http.js
@@ -2346,10 +2346,10 @@ module.exports = {
 					}),
 					
 					match: doodad.OVERRIDE(function match(request, requestUrl, handlerOptions) {
-						const urlPath = tools.trim(requestUrl.toArray(), '');
+						const urlPath = requestUrl.toArray({isPath: true, trim: true});
 						const urlPathLen = urlPath.length;
 
-						const basePath = tools.trim(this.baseUrl.toArray(), '');
+						const basePath = this.baseUrl.toArray({isPath: true, trim: true});
 						const basePathLen = basePath.length;
 
 						let weight = 0,    // weight
